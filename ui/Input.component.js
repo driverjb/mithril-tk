847ecbb9fabf312813dddd95021fdf24f3d2bcb7
driverjb <driverjb09@gmail.com>
2020-11-23 16:36:10 -0500

    annoying

--- ui/Input.component.js
+++ ui/Input.component.js
@@ -4,12 +4,14 @@ export const Input = {
   view: ({ attrs }) => {
     /** @type {import('../util/Field.class').Field} */
     let field = attrs.field;
+    let cls = attrs.class;
+    delete attrs.class;
     return m('input', {
       oninput: ({ target: { value } }) => (field.value = value),
       class:
         `${
           field.requiresValidation ? (field.isValid ? field.successClass : field.failClass) : ''
-        } ${attrs.class}${attrs.fail ? ` ${field.failClass}` : ''}` + ` ${attrs.class}`,
+        } ${attrs.class}${attrs.fail ? ` ${field.failClass}` : ''}` + ` ${cls}`,
       placeholder: field.name,
       value: field.value,
       ...attrs
